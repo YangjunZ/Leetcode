154d2ffbf661c05b7751c34bfe301ceb0e14bd9a
Yangjun <mimzhan@gmail.com>
2015-01-27 23:49:53 +0800

    Palindrome Partitioning

---
+++ Palindrome_Partitioning.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+// https://oj.leetcode.com/problems/palindrome-partitioning/
+
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+         
+    }
+};
+
+int main()
+{
+	cout << "hello world"<<endl;
+	return 0;
+}
